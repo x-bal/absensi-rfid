2d786d0adf110b1db496c9bed7f70a2374151868
MXbal <imshroud584@gmail.com>
2022-03-08 14:03:20 +0700

    Update Beta v.1.3

--- resources/views/absensi-staff/generate.blade.php
+++ resources/views/absensi-staff/generate.blade.php
@@ -4,6 +4,12 @@
             <th>No</th>
             <th>Westin ID</th>
             <th>Nama</th>
+            @if($act == 'export' && $from != '' && $to != '')
+            @foreach (Carbon\CarbonPeriod::create($from, $to) as $date)
+            <th>{{ $date->format('d/m/y') }}</th>
+            @endforeach
+            @endifach
+            @endif
             <th>Hadir</th>
             <th>Hadir Via Zoom</th>
             <th>Sakit</th>
@@ -28,6 +34,13 @@
             <td>{{ $loop->iteration }}</td>
             <td>{{ $stf->nik }}</td>
             <td>{{ $stf->nama }}</td>
+            @if($act == 'export' && $from != '' && $to != '')
+            @foreach (Carbon\CarbonPeriod::create($from, $to) as $date)
+            <td class="text-center">
+                {{ $stf->absensiStaff->where('created_at', '>=', $date->format('Y-m-d 00:00:00'))->where('created_at', '<', $date->addDay(1)->format('Y-m-d 00:00:00'))->first()->status_hadir ?? '-' }}
+            </td>
+            @endforeach
+            @endif
             <td class="text-center">
                 {{ $stf->absensiStaff->whereBetween('created_at', [$from, Carbon\Carbon::parse($to)->addDay(1)->format('Y-m-d 00:00:00')])->where('status_hadir', 'Hadir')->count() }}
             </td>
@@ -54,28 +67,19 @@
 
             $alpa += $stf->absensiStaff->whereBetween('created_at', [$from, Carbon\Carbon::parse($to)->addDay(1)->format('Y-m-d 00:00:00')])->where('status_hadir', 'Alpa')->count();
             @endphp
-
-            @if($act != 'export')
-            <td>
-                <div class="d-flex">
-                    <a href="{{ route('siswa.edit', $stf->id) }}" class="btn btn-sm btn-success mr-1"><i class="fas fa-edit"></i></a>
-                </div>
-            </td>
-            @endif
         </tr>
         @endforeach
     </tbody>
+    @if($act == 'export' && $from != '' && $to != '')
     <tfoot>
         <tr>
-            <th colspan="3">Total</th>
+            <th colspan="{{ Carbon\Carbon::parse(request('to'))->diffInDays(Carbon\Carbon::parse(request('from'))) + 4 }}">Total</th>
             <th class="text-center">{{ $hadir }}</th>
             <th class="text-center">{{ $zoom }}</th>
             <th class="text-center">{{ $sakit }}</th>
             <th class="text-center">{{ $ijin }}</th>
             <th class="text-center">{{ $alpa }}</th>
-            @if($act != 'export')
-            <th>#</th>
-            @endif
         </tr>
     </tfoot>
+    @endif
 </table>
--- resources/views/absensi/generate.blade.php
+++ resources/views/absensi/generate.blade.php
@@ -14,9 +14,6 @@
             <th>Sakit</th>
             <th>Ijin</th>
             <th>Alpa</th>
-            @if($act != 'export')
-            <th>Action</th>
-            @endif
         </tr>
     </thead>
 
@@ -36,7 +33,9 @@
             <td>{{ $sw->nama }}</td>
             @if($act == 'export' && $from != '' && $to != '')
             @foreach (Carbon\CarbonPeriod::create($from, $to) as $date)
-            <th class="text-center">{{ $sw->absensi->where('created_at', '>=', $date->format('Y-m-d 00:00:00'))->where('created_at', '<', $date->addDay(1)->format('Y-m-d 00:00:00'))->first()->status_hadir ?? '-' }}</th>
+            <td class="text-center">
+                {{ $sw->absensi->where('created_at', '>=', $date->format('Y-m-d 00:00:00'))->where('created_at', '<', $date->addDay(1)->format('Y-m-d 00:00:00'))->first()->status_hadir ?? '-' }}
+            </td>
             @endforeach
             @endif
             <td class="text-center">
@@ -65,29 +64,20 @@
 
             $alpa += $sw->absensi->whereBetween('created_at', [$from, Carbon\Carbon::parse($to)->addDay(1)->format('Y-m-d 00:00:00')])->where('status_hadir', 'Alpa')->count();
             @endphp
-
-            @if($act != 'export')
-            <td>
-                <div class="d-flex">
-                    <a href="{{ route('siswa.export', $sw->id) }}" class="btn btn-sm btn-info mr-1"><i class="fas fa-download"></i></a>
-                </div>
-            </td>
-            @endif
         </tr>
         @endforeach
     </tbody>
-    <!-- <tfoot>
+    @if($act == 'export' && $from != '' && $to != '')
+    <tfoot>
         <tr>
-            <th colspan="3">Total</th>
+            <th colspan="{{ Carbon\Carbon::parse(request('to'))->diffInDays(Carbon\Carbon::parse(request('from'))) + 4 }}">Total</th>
             <th class="text-center">{{ $hadir }}</th>
             <th class="text-center">{{ $zoom }}</th>
             <th class="text-center">{{ $sakit }}</th>
             <th class="text-center">{{ $ijin }}</th>
             <th class="text-center">{{ $alpa }}</th>
-            @if($act != 'export')
-            <th class="text-center">#</th>
-            @endif
         </tr>
-    </tfoot> -->
+    </tfoot>
+    @endif
     @endif
 </table>
